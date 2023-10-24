f39229b9b60e9f621aae6b41237d94ab9d4ee3bd
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:17:05 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_quadratic_equation_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Quadratic Equation Calculator by Calculator.iO";
 
 function display_ci_quadratic_equation_calculator(){
